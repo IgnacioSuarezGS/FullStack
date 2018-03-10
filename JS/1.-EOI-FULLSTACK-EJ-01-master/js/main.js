44265654ec1d8bb306998ba7885ec41c43bc405e
fausrguez <faustinorodriguezfleitas@gmail.com>
2018-03-10 13:35:10 +0000

    ejercicio1

--- JS/1.-EOI-FULLSTACK-EJ-01-master/js/main.js
+++ JS/1.-EOI-FULLSTACK-EJ-01-master/js/main.js
@@ -10,7 +10,7 @@ function quienEsMayorDeLosTres(p1, p2, p3) {
     } else if (p3.age > p1.age && p3.age > p2.age) {
         return p3.name;
     } else {
-       return 'Chungo';
+        return 'Chungo';
     }
 }
 
@@ -19,7 +19,7 @@ function quienEsMayorDeLosTres(p1, p2, p3) {
 // NOTA: las mayúsculas y minúsculas no se consideran distintas
 function hayAlgunCorreoRepetido(p1, p2, p3) {
     // toLowerCase
-    if(p1.email === p2.email || p2.email === p3.email || p3.email === p1.email){
+    if (p1.email === p2.email || p2.email === p3.email || p3.email === p1.email) {
         return true;
     } else {
         return false;
@@ -30,9 +30,7 @@ function hayAlgunCorreoRepetido(p1, p2, p3) {
 function elUsuarioEsMayorQueLosTres(p1, p2, p3) {
     userAge = parseInt(prompt('¿Cuántos años tienes MADAFAKA?'));
     isNumber = isNaN(userAge);
-    console.log(userAge);
-    console.log(isNumber);
-    if(isNumber === true){
+    if (isNumber === true) {
         alert('Chacho! Khe hiciste Mostro?');
     } else if (userAge > p1.age && userAge > p2.age && userAge > p3.age) {
         return true;
@@ -42,14 +40,40 @@ function elUsuarioEsMayorQueLosTres(p1, p2, p3) {
 }
 
 // Devuelve el número de los que tienen correo acabado en gmail.com
-// RESTRICCIÓN: Resuélvelo usando los métodos .indexOf() ó .lastIndexOf()
-// RESTRICCIÓN: Resuélvelo usando el método .search()
-// RESTRICCIÓN: Resuélvelo usando los métodos .substring() ó substr()
-// RESTRICCIÓN: Plantea otra forma de solucionarlo que no use ninguno de los métodos de arriba
 // Deja descomentada solo una de las soluciones
 function cuantosTienenCorreoDeGmailDotCom(p1, p2, p3) {
+    // RESTRICCIÓN: Resuélvelo usando los métodos .indexOf() ó .lastIndexOf()
 
-    // OPCION UNO
+    // let gmails = 0;
+    // if(p1.email.indexOf("gmail.com") > 0) gmails++;
+    // if(p2.email.indexOf("gmail.com") > 0) gmails++;
+    // if(p3.email.indexOf("gmail.com") > 0) gmails++;
+    // return String(gmails);
+
+    // RESTRICCIÓN: Resuélvelo usando el método .search()
+
+    // let gmails = 0;
+    // if(p1.email.search("gmail.com") > 0) gmails++;
+    // if(p2.email.search("gmail.com") > 0) gmails++;
+    // if(p3.email.search("gmail.com") > 0) gmails++;
+    // return String(gmails);
+
+    // RESTRICCIÓN: Resuélvelo usando los métodos .substring() ó substr()
+
+    let gmails = 0;
+    let gmail = "gmail.com".length
+    let p1Mail = p1.email.length;
+    let p2Mail = p2.email.length;
+    let p3Mail = p3.email.length;
+    if (p1.email.substring(p1Mail - gmail) == "gmail.com") gmails++;
+    if (p2.email.substring(p2Mail - gmail) == "gmail.com") gmails++;
+    if (p3.email.substring(p3Mail - gmail) == "gmail.com") gmails++;
+    return String(gmails);
+
+
+    // RESTRICCIÓN: Plantea otra forma de solucionarlo que no use ninguno de los métodos de arriba
+
+
     // numeroEmails = 0;
 
     // if(p1.email.includes("gmail.com")){
@@ -61,11 +85,12 @@ function cuantosTienenCorreoDeGmailDotCom(p1, p2, p3) {
     // if(p3.email.includes("gmail.com")){
     //     numeroEmails++;
     // }
-    
+
     // if (numeroEmails != 0){
     //     return numeroEmails;
     // } else {
     //     return "0";
     // }
+
 
 }
